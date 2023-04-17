5b4eaa0dcbf9b7ad4937ae77eaf74af1e3c2ef34
C00MAR <moureau.marc78@gmail.com>
2023-04-17 15:48:12 +0200

    ✨ Tap Bar ✨

---
+++ src/components/TapBar.vue
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<template>
+    <div class="tapbar_box">
+        <div class="tapbar_category active">
+            <img src="asset\icon\icon_like_active.png" alt="">
+            <p>Suivis</p>
+        </div>
+        <div class="tapbar_category">
+            <img src="asset\icon\icon_decouvrir.png" alt="">
+            <p>Découvrir</p>
+        </div>
+        <div class="tapbar_category">
+            <img src="asset\icon\icon_parcourir.png" alt="">
+            <p>Parcourir</p>
+        </div>
+        <div class="tapbar_category">
+            <img src="asset\icon\icon_loupe.png" alt="">
+            <p>Rechercher</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .tapbar_box{
+        background-color: #0e0e10;
+        position: absolute;
+        bottom: 0;
+        font-size: 17px;
+        width: 100vw;
+        height: 10vh;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        padding: 0 5vw;
+        .tapbar_category{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 5px;
+            font-size: 12px;
+            
+            img{
+                width: 32px;
+                height: 32px;
+            }
+        }
+    }
+    
+    .tapbar_box .active{
+        color: var(--color-light-purple);
+    }
+</style>
