9418b0b7cbc8124aae96798d258f026eadfc07c0
C00MAR <moureau.marc78@gmail.com>
2023-04-17 16:16:45 +0200

    ✨ Top Bar ✨

--- src/components/ProfilBar.vue
+++ src/components/ProfilBar.vue
@@ -1,8 +1,56 @@
 <template>
-    <div>
-        
+    <div class="profil_box">
+        <div class="profil_icon">
+            <img src="asset\icon\Account-Avatar.png" alt="" class="pp">
+            <img src="asset\icon\icon_coment.png" alt="" class="icon">
+            <img src="asset\icon\icon_inbox.png" alt="" class="icon">
+            <img src="asset\icon\icon_cast.png" alt="" class="icon">
+        </div>
+        <div class="profil_create">
+            <button class="secondary">
+                <img src="asset\icon\icon_diffusion.png" alt="">
+                Créer
+            </button>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.profil_box{
+    width: 100vw;
+    height: 15vh;
+    position: absolute;
+    top: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 5vw;
+    align-items: center;
+    .profil_icon{
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        .pp{
+            width: 40px;
+            height: 40px;
+        }
+        .icon{
+            width: 32px;
+            height: 32px;
+        }
+
+    }
+    .profil_create{
+        .secondary{
+            border-radius: 50px;
+            display: flex;
+            align-items: center;
+            gap: 5px;
+            img{
+                width: 32px;
+                height: 32px;
+            }
+        }
+    }
+}
 </style>
